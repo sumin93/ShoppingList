20b9cb781936be9b10fdfe39222911f4db78e377
Andrew Sumin <suminfx@mail.ru>
2021-06-07 21:02:20 +0300

    create adapter

---
+++ app/src/main/java/com/sumin/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,37 @@
+package com.sumin.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sumin.shoppinglist.R
+import com.sumin.shoppinglist.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+        viewHolder.view.setOnLongClickListener {
+            true
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+}
