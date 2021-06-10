7d6215016dafaf55a15a7b078869fcb60628f801
Andrew Sumin <suminfx@mail.ru>
2021-06-10 16:51:18 +0300

    DiffUtil implementation

--- app/src/main/java/com/sumin/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/sumin/shoppinglist/presentation/ShopListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sumin.shoppinglist.R
 import com.sumin.shoppinglist.domain.ShopItem
@@ -15,8 +16,10 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
+            val callback = ShopListDiffCallback(shopList, value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
 
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
---
+++ app/src/main/java/com/sumin/shoppinglist/presentation/ShopListDiffCallback.kt
@@ -0,0 +1,30 @@
+package com.sumin.shoppinglist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.sumin.shoppinglist.domain.ShopItem
+
+class ShopListDiffCallback(
+    private val oldList: List<ShopItem>,
+    private val newList: List<ShopItem>,
+): DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem == newItem
+    }
+}
