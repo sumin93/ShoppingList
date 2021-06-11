c2e684c83e7b35e9bb4de332b89916b0680d9d8d
Andrew Sumin <suminfx@mail.ru>
2021-06-11 11:33:55 +0300

    prepare ShopItemActivity.kt layout

---
+++ app/src/main/java/com/sumin/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.sumin.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sumin.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
