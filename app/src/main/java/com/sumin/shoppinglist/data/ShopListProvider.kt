261dd5551cece5be83c7f4d38daea4065dee3ea7
a.sumin <sumin93@mail.ru>
2022-02-24 00:27:32 +0300

    add Uri matcher

--- app/src/main/java/com/sumin/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/sumin/shoppinglist/data/ShopListProvider.kt
@@ -2,11 +2,17 @@ package com.sumin.shoppinglist.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShopListProvider : ContentProvider() {
+
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI("com.sumin.shoppinglist", "shop_items", GET_SHOP_ITEMS_QUERY)
+        addURI("com.sumin.shoppinglist", "shop_items/#", GET_SHOP_ITEM_BY_ID_QUERY)
+    }
 
     override fun onCreate(): Boolean {
         return true
@@ -19,7 +25,13 @@ class ShopListProvider : ContentProvider() {
         selectionArgs: Array<out String>?,
         sortOrder: String?
     ): Cursor? {
-        Log.d("ShopListProvider", "query $uri")
+        val code = uriMatcher.match(uri)
+        when (code) {
+            GET_SHOP_ITEMS_QUERY -> {
+
+            }
+        }
+        Log.d("ShopListProvider", "query $uri code $code")
         return null
     }
 
@@ -42,5 +54,11 @@ class ShopListProvider : ContentProvider() {
         selectionArgs: Array<out String>?
     ): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+
+        private const val GET_SHOP_ITEMS_QUERY = 100
+        private const val GET_SHOP_ITEM_BY_ID_QUERY = 101
     }
 }
